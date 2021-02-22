8bc697cfbcbefe2bfd6a0cb2d15cb7c1df2f4a61
Ashraff Hatz <ashraff.hatz@gmail.com>
2021-02-22 15:29:02 +0000

    fixed ast bugs for feature syntax

--- src/ASTBuilder.java
+++ src/ASTBuilder.java
@@ -1,7 +1,7 @@
 import ast.ClassNode;
 import ast.ProgramNode;
 import ast.*;
-import java.util.ArrayList;
+import java.util.*;
 
 public class ASTBuilder extends CoolParserBaseVisitor<Tree> {
 
@@ -17,47 +17,53 @@ public class ASTBuilder extends CoolParserBaseVisitor<Tree> {
 
   @Override
   public Tree visitCoolClass(CoolParser.CoolClassContext ctx) {
-    String superType = (ctx.INHERITS() == null) ? "Object" : ctx.TYPEID(1);
+    String superType = (ctx.INHERITS() == null) ? "Object" : ctx.TYPEID(1).getSymbol().getText();
 
     Symbol name = StringTable.idtable.addString(ctx.TYPEID(0).getSymbol().getText());
     Symbol parent = StringTable.idtable.addString(superType);
     Symbol filename = StringTable.idtable.addString(Flags.in_filename);
 
-    ClassNode classNode = new ClassNode(lineNum(ctx), name, parent, filename);
+    ClassNode classNode = new ClassNode(ctx.CLASS().getSymbol().getLine(), name, parent, filename);
     for (CoolParser.FeatureContext c : ctx.feature()) {
       classNode.add((FeatureNode) visit(c));
     }
-
     return classNode;
-
   }
 
   @Override
-  public Tree visitMethodNode(CoolParser.ProgramContext ctx) {
+  public Tree visitFeature(CoolParser.FeatureContext ctx) {
+    if (ctx.PARENT_OPEN() == null) { // isAttribute
+      return visitAttributeNode(ctx);
+    }
+    return visitMethodNode(ctx); // isMethod
+  }
+
+  public Tree visitMethodNode(CoolParser.FeatureContext ctx) {
+
     List<FormalNode> formalNodes = new ArrayList<>();
     Symbol name = StringTable.idtable.addString(ctx.OBJECTID().getSymbol().getText());
     Symbol returnType = StringTable.idtable.addString(ctx.TYPEID().getSymbol().getText());
 
-    MethodNode methodNode = new MethodNode(lineNum(ctx), name, returnType, (ExpressionNode) visit(ctx.expr()));
+    MethodNode methodNode = new MethodNode(ctx.OBJECTID().getSymbol().getLine(), name, formalNodes, returnType,
+        (ExpressionNode) visit(ctx.expr()));
 
     for (CoolParser.FormalContext c : ctx.formal()) {
       formalNodes.add((FormalNode) visit(c));
     }
 
     return methodNode;
-
   }
 
-  @Override
-  public Tree visitAttributeNode(CoolParser.ProgramContext ctx) {
+  public Tree visitAttributeNode(CoolParser.FeatureContext ctx) {
     Symbol name = StringTable.idtable.addString(ctx.OBJECTID().getSymbol().getText());
     Symbol type = StringTable.idtable.addString(ctx.TYPEID().getSymbol().getText());
 
-    // unsure if ternary should return null if true
-    ExpressionNode expr = (ctx.ASSIGN_OPERATOR() == null) ? null : new ExpressionNode(lineNum(ctx));
+    ExpressionNode expr = (ctx.ASSIGN_OPERATOR() == null) ? new NoExpressionNode(0)
+        : (ExpressionNode) visit(ctx.expr());
 
-    AttributeNode attrNode = new AttributeNode(lineNum(ctx), name, type, ctx.visit(expr));
+    AttributeNode attrNode = new AttributeNode(ctx.OBJECTID().getSymbol().getLine(), name, type, expr);
+
     return attrNode;
-
   }
+
 }

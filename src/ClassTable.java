f6cacfcf23fdc9ed99bf72603fec9130988f512a
Ashraff Hatz <ashraff.hatz@gmail.com>
2021-03-03 18:52:38 +0000

    implement inheritance graph

--- src/ClassTable.java
+++ src/ClassTable.java
@@ -2,198 +2,222 @@ import ast.*;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.*;
 
+class InheritanceGraph {
 
-/**
- * This class may be used to contain the semantic information such as
- * the inheritance graph.  You may use it or not as you like: it is only
- * here to provide a container for the supplied methods.
- */
-class ClassTable {
-    /**
-     * Creates data structures representing basic Cool classes (Object,
-     * IO, Int, Bool, String).  Please note: as is this method does not
-     * do anything useful; you will need to edit it to make if do what
-     * you want.
+  private InheritanceGraphNode root;
+
+  public InheritanceGraphNode getRoot() {
+    return this.root;
+  }
+
+  public void setRoot(InheritanceGraphNode newRoot) {
+    this.root = newRoot;
+  }
+
+  public InheritanceGraphNode findParentNode(Symbol parent) {
+    LinkedList<InheritanceGraphNode> queue = new LinkedList<InheritanceGraphNode>();
+    queue.add(this.root); // start at root
+
+    InheritanceGraphNode current;
+
+    while (queue.size() > 0) {
+      current = queue.pollFirst();
+      Symbol currentClass = current.getValue().getName();
+
+      if (currentClass.getName().equals(parent.getName())) {
+        return current;
+      }
+
+      for (InheritanceGraphNode child : current.getChildren()) {
+        queue.add(child);
+      }
+    }
+
+    /*
+     * THROW ERROR IF NOT VALID PARENT (INVALID INHERITANCE)
      */
-    private void installBasicClasses() {
-        Symbol filename
-                = StringTable.stringtable.addString("<basic class>");
+    return null;
+  }
 
-        LinkedList<FormalNode> formals;
+  public void print() {
+    LinkedList<InheritanceGraphNode> queue = new LinkedList<InheritanceGraphNode>();
+    queue.add(this.root); // start at root
 
-        // The following demonstrates how to create dummy parse trees to
-        // refer to basic Cool classes.  There's no need for method
-        // bodies -- these are already built into the runtime system.
+    InheritanceGraphNode current;
 
-        // IMPORTANT: The results of the following expressions are
-        // stored in local variables.  You will want to do something
-        // with those variables at the end of this method to make this
-        // code meaningful.
+    while (queue.size() > 0) {
+      current = queue.pollFirst();
+      Symbol currentClass = current.getValue().getName();
 
-        // The Object class has no parent class. Its methods are
-        //        cool_abort() : Object    aborts the program
-        //        type_name() : Str        returns a string representation
-        //                                 of class name
-        //        copy() : SELF_TYPE       returns a copy of the object
+      System.out.println("Current Node: " + currentClass.getName());
 
-        ClassNode Object_class =
-                new ClassNode(0,
-                        TreeConstants.Object_,
-                        TreeConstants.No_class,
-                        filename);
+      for (InheritanceGraphNode child : current.getChildren()) {
+        System.out.print(child.getValue().getName().getName() + " --- ");
+        queue.add(child);
+      }
 
-        Object_class.add(new MethodNode(0,
-                TreeConstants.cool_abort,
-                new LinkedList<FormalNode>(),
-                TreeConstants.Object_,
-                new NoExpressionNode(0)));
-
-        Object_class.add(new MethodNode(0,
-                TreeConstants.type_name,
-                new LinkedList<FormalNode>(),
-                TreeConstants.Str,
-                new NoExpressionNode(0)));
-
-        Object_class.add(new MethodNode(0,
-                TreeConstants.copy,
-                new LinkedList<FormalNode>(),
-                TreeConstants.SELF_TYPE,
-                new NoExpressionNode(0)));
-
-        // The IO class inherits from Object. Its methods are
-        //        out_string(Str) : SELF_TYPE  writes a string to the output
-        //        out_int(Int) : SELF_TYPE      "    an int    "  "     "
-        //        in_string() : Str            reads a string from the input
-        //        in_int() : Int                "   an int     "  "     "
-
-        ClassNode IO_class =
-                new ClassNode(0,
-                        TreeConstants.IO,
-                        TreeConstants.Object_,
-                        filename);
-
-        formals = new LinkedList<FormalNode>();
-        formals.add(
-                new FormalNode(0,
-                        TreeConstants.arg,
-                        TreeConstants.Str));
-
-        IO_class.add(new MethodNode(0,
-                TreeConstants.out_string,
-                formals,
-                TreeConstants.SELF_TYPE,
-                new NoExpressionNode(0)));
-
-
-        formals = new LinkedList<FormalNode>();
-        formals.add(
-                new FormalNode(0,
-                        TreeConstants.arg,
-                        TreeConstants.Int));
-        IO_class.add(new MethodNode(0,
-                TreeConstants.out_int,
-                formals,
-                TreeConstants.SELF_TYPE,
-                new NoExpressionNode(0)));
-
-        IO_class.add(new MethodNode(0,
-                TreeConstants.in_string,
-                new LinkedList<FormalNode>(),
-                TreeConstants.Str,
-                new NoExpressionNode(0)));
-
-        IO_class.add(new MethodNode(0,
-                TreeConstants.in_int,
-                new LinkedList<FormalNode>(),
-                TreeConstants.Int,
-                new NoExpressionNode(0)));
-
-        // The Int class has no methods and only a single attribute, the
-        // "val" for the integer.
-
-        ClassNode Int_class =
-                new ClassNode(0,
-                        TreeConstants.Int,
-                        TreeConstants.Object_,
-                        filename);
-
-        Int_class.add(new AttributeNode(0,
-                TreeConstants.val,
-                TreeConstants.prim_slot,
-                new NoExpressionNode(0)));
-
-        // Bool also has only the "val" slot.
-        ClassNode Bool_class =
-                new ClassNode(0,
-                        TreeConstants.Bool,
-                        TreeConstants.Object_,
-                        filename);
-
-        Bool_class.add(new AttributeNode(0,
-                TreeConstants.val,
-                TreeConstants.prim_slot,
-                new NoExpressionNode(0)));
-
-        // The class Str has a number of slots and operations:
-        //       val                              the length of the string
-        //       str_field                        the string itself
-        //       length() : Int                   returns length of the string
-        //       concat(arg: Str) : Str           performs string concatenation
-        //       substr(arg: Int, arg2: Int): Str substring selection
-
-        ClassNode Str_class =
-                new ClassNode(0,
-                        TreeConstants.Str,
-                        TreeConstants.Object_,
-                        filename);
-        Str_class.add(new AttributeNode(0,
-                TreeConstants.val,
-                TreeConstants.Int,
-                new NoExpressionNode(0)));
-
-        Str_class.add(new AttributeNode(0,
-                TreeConstants.str_field,
-                TreeConstants.prim_slot,
-                new NoExpressionNode(0)));
-        Str_class.add(new MethodNode(0,
-                TreeConstants.length,
-                new LinkedList<FormalNode>(),
-                TreeConstants.Int,
-                new NoExpressionNode(0)));
-
-        formals = new LinkedList<FormalNode>();
-        formals.add(new FormalNode(0,
-                TreeConstants.arg,
-                TreeConstants.Str));
-        Str_class.add(new MethodNode(0,
-                TreeConstants.concat,
-                formals,
-                TreeConstants.Str,
-                new NoExpressionNode(0)));
-
-        formals = new LinkedList<FormalNode>();
-        formals.add(new FormalNode(0,
-                TreeConstants.arg,
-                TreeConstants.Int));
-        formals.add(new FormalNode(0,
-                TreeConstants.arg2,
-                TreeConstants.Int));
-
-        Str_class.add(new MethodNode(0,
-                TreeConstants.substr,
-                formals,
-                TreeConstants.Str,
-                new NoExpressionNode(0)));
-
-	/* Do somethind with Object_class, IO_class, Int_class,
-           Bool_class, and Str_class here */
-
+      System.out.println('\n');
     }
-
-    public ClassTable(List<ClassNode> cls) {
-    	/* fill this in */
-    }
+  }
 }
 
-    
+class InheritanceGraphNode {
+  private ClassNode value;
+  private List<InheritanceGraphNode> children;
+
+  public InheritanceGraphNode(ClassNode value) {
+    this.value = value;
+    this.children = new ArrayList<>();
+  }
+
+  public InheritanceGraphNode addChild(ClassNode newValue) {
+    InheritanceGraphNode newChild = new InheritanceGraphNode(newValue);
+    children.add(newChild);
+    return newChild;
+  }
+
+  public ClassNode getValue() {
+    return this.value;
+  }
+
+  public List<InheritanceGraphNode> getChildren() {
+    return this.children;
+  }
+}
+
+/**
+ * This class may be used to contain the semantic information such as the
+ * inheritance graph. You may use it or not as you like: it is only here to
+ * provide a container for the supplied methods.
+ */
+class ClassTable {
+
+  InheritanceGraph inheritanceGraph = new InheritanceGraph();
+
+  public ClassTable(List<ClassNode> userDefinedClasses) {
+    this.installBasicClasses();
+
+    for (ClassNode userClass : userDefinedClasses) {
+      Symbol parentClass = userClass.getParent();
+
+      InheritanceGraphNode parentNode = inheritanceGraph.findParentNode(parentClass);
+      InheritanceGraphNode newClass = new InheritanceGraphNode(userClass);
+      parentNode.getChildren().add(newClass);
+    }
+
+    this.inheritanceGraph.print();
+  }
+
+  /**
+   * Creates data structures representing basic Cool classes (Object, IO, Int,
+   * Bool, String). Please note: as is this method does not do anything useful;
+   * you will need to edit it to make if do what you want.
+   */
+  private void installBasicClasses() {
+
+    Symbol filename = StringTable.idtable.addString(Flags.in_filename);
+
+    LinkedList<FormalNode> formals;
+
+    // The following demonstrates how to create dummy parse trees to
+    // refer to basic Cool classes. There's no need for method
+    // bodies -- these are already built into the runtime system.
+
+    // IMPORTANT: The results of the following expressions are
+    // stored in local variables. You will want to do something
+    // with those variables at the end of this method to make this
+    // code meaningful.
+
+    // The Object class has no parent class. Its methods are
+    // cool_abort() : Object aborts the program
+    // type_name() : Str returns a string representation
+    // of class name
+    // copy() : SELF_TYPE returns a copy of the object
+
+    ClassNode Object_class = new ClassNode(0, TreeConstants.Object_, TreeConstants.No_class, filename);
+
+    Object_class.add(new MethodNode(0, TreeConstants.cool_abort, new LinkedList<FormalNode>(), TreeConstants.Object_,
+        new NoExpressionNode(0)));
+
+    Object_class.add(new MethodNode(0, TreeConstants.type_name, new LinkedList<FormalNode>(), TreeConstants.Str,
+        new NoExpressionNode(0)));
+
+    Object_class.add(new MethodNode(0, TreeConstants.copy, new LinkedList<FormalNode>(), TreeConstants.SELF_TYPE,
+        new NoExpressionNode(0)));
+
+    // The IO class inherits from Object. Its methods are
+    // out_string(Str) : SELF_TYPE writes a string to the output
+    // out_int(Int) : SELF_TYPE " an int " " "
+    // in_string() : Str reads a string from the input
+    // in_int() : Int " an int " " "
+
+    ClassNode IO_class = new ClassNode(0, TreeConstants.IO, TreeConstants.Object_, filename);
+
+    formals = new LinkedList<FormalNode>();
+    formals.add(new FormalNode(0, TreeConstants.arg, TreeConstants.Str));
+
+    IO_class
+        .add(new MethodNode(0, TreeConstants.out_string, formals, TreeConstants.SELF_TYPE, new NoExpressionNode(0)));
+
+    formals = new LinkedList<FormalNode>();
+    formals.add(new FormalNode(0, TreeConstants.arg, TreeConstants.Int));
+    IO_class.add(new MethodNode(0, TreeConstants.out_int, formals, TreeConstants.SELF_TYPE, new NoExpressionNode(0)));
+
+    IO_class.add(new MethodNode(0, TreeConstants.in_string, new LinkedList<FormalNode>(), TreeConstants.Str,
+        new NoExpressionNode(0)));
+
+    IO_class.add(new MethodNode(0, TreeConstants.in_int, new LinkedList<FormalNode>(), TreeConstants.Int,
+        new NoExpressionNode(0)));
+
+    // The Int class has no methods and only a single attribute, the
+    // "val" for the integer.
+
+    ClassNode Int_class = new ClassNode(0, TreeConstants.Int, TreeConstants.Object_, filename);
+
+    Int_class.add(new AttributeNode(0, TreeConstants.val, TreeConstants.prim_slot, new NoExpressionNode(0)));
+
+    // Bool also has only the "val" slot.
+    ClassNode Bool_class = new ClassNode(0, TreeConstants.Bool, TreeConstants.Object_, filename);
+
+    Bool_class.add(new AttributeNode(0, TreeConstants.val, TreeConstants.prim_slot, new NoExpressionNode(0)));
+
+    // The class Str has a number of slots and operations:
+    // val the length of the string
+    // str_field the string itself
+    // length() : Int returns length of the string
+    // concat(arg: Str) : Str performs string concatenation
+    // substr(arg: Int, arg2: Int): Str substring selection
+
+    ClassNode Str_class = new ClassNode(0, TreeConstants.Str, TreeConstants.Object_, filename);
+    Str_class.add(new AttributeNode(0, TreeConstants.val, TreeConstants.Int, new NoExpressionNode(0)));
+
+    Str_class.add(new AttributeNode(0, TreeConstants.str_field, TreeConstants.prim_slot, new NoExpressionNode(0)));
+    Str_class.add(new MethodNode(0, TreeConstants.length, new LinkedList<FormalNode>(), TreeConstants.Int,
+        new NoExpressionNode(0)));
+
+    formals = new LinkedList<FormalNode>();
+    formals.add(new FormalNode(0, TreeConstants.arg, TreeConstants.Str));
+    Str_class.add(new MethodNode(0, TreeConstants.concat, formals, TreeConstants.Str, new NoExpressionNode(0)));
+
+    formals = new LinkedList<FormalNode>();
+    formals.add(new FormalNode(0, TreeConstants.arg, TreeConstants.Int));
+    formals.add(new FormalNode(0, TreeConstants.arg2, TreeConstants.Int));
+
+    Str_class.add(new MethodNode(0, TreeConstants.substr, formals, TreeConstants.Str, new NoExpressionNode(0)));
+
+    /*
+     * Add Object_class, IO_class, Int_class, Bool_class, and Str_class to the
+     * inheritance graph
+     */
+
+    InheritanceGraphNode Object_node = new InheritanceGraphNode(Object_class);
+    Object_node.addChild(IO_class);
+    Object_node.addChild(Str_class);
+    Object_node.addChild(Int_class);
+    Object_node.addChild(Bool_class);
+    this.inheritanceGraph.setRoot(Object_node);
+  }
+
+}
